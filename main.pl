da4ad5620fd5a2de9c3b030d09830f5aeb0bbbaf
Avery Brinkman <71273469+Avery-Brinkman@users.noreply.github.com>
2021-11-12 13:05:41 -0500

    apply_lr

--- main.pl
+++ main.pl
@@ -2,3 +2,6 @@ apply(add, X, Y, Result) :- Result is X + Y, !.
 apply(subtract, X, Y, Result) :- Result is X - Y, !.
 apply(multiply, X, Y, Result) :- Result is X * Y, !.
 apply(divide, X, Y, Result) :- Result is X / Y, !.
+
+apply_lr([], [Result], Result).
+apply_lr([Op|OpTail], [X, Y |NumTail], FinalResult) :- apply(Op, X, Y, Result), apply_lr(OpTail, [Result|NumTail], FinalResult).	
