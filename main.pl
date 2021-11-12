1ae1b296bc5f0d107949b9c325d4772ad95ccb3b
Avery Brinkman <71273469+Avery-Brinkman@users.noreply.github.com>
2021-11-12 12:52:22 -0500

    apply/4

--- main.pl
+++ main.pl
@@ -1,1 +1,4 @@
-
+apply(add, X, Y, Result) :- Result is X + Y, !.
+apply(subtract, X, Y, Result) :- Result is X - Y, !.
+apply(multiply, X, Y, Result) :- Result is X * Y, !.
+apply(divide, X, Y, Result) :- Result is X / Y, !.
