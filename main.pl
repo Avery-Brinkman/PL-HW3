162ad3c3f97bb52ef97a932beff4f3c8f08a0c2e
Avery Brinkman <71273469+Avery-Brinkman@users.noreply.github.com>
2021-11-13 13:30:42 -0500

    allLists functor (using populate_list)

--- main.pl
+++ main.pl
@@ -5,3 +5,8 @@ apply(divide, X, Y, Result) :- Result is X / Y, !.
 
 apply_lr([], [Result], Result).
 apply_lr([Op|OpTail], [X, Y |NumTail], FinalResult) :- apply(Op, X, Y, Result), apply_lr(OpTail, [Result|NumTail], FinalResult).	
+
+populate_list([], _, []).
+populate_list([_|Xs], Elements, NewList) :- append(_, [E|_], Elements), populate_list(Xs, Elements, NewTail), append([E], NewTail, NewList).
+
+allLists(Num, Elements, Result) :- length(EmptyList, Num), populate_list(EmptyList, Elements, Result).
