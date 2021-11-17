1436647c044279875c79d0c41753779a5ecb86f9
Avery Brinkman <71273469+Avery-Brinkman@users.noreply.github.com>
2021-11-17 13:52:13 -0500

    combo (buggy)

--- main.pl
+++ main.pl
@@ -9,4 +9,7 @@ apply_lr([Op|OpTail], [X, Y |NumTail], FinalResult) :- apply(Op, X, Y, Result), 
 populate_list([], _, []).
 populate_list([_|Xs], Elements, NewList) :- append(_, [E|_], Elements), populate_list(Xs, Elements, NewTail), append([E], NewTail, NewList).
 
-allLists(Num, Elements, Result) :- length(EmptyList, Num), populate_list(EmptyList, Elements, Result).
+allLists(Num, Elements, Result) :- ground(Elements), !, length(EmptyList, Num), populate_list(EmptyList, Elements, Result).
+allLists(Num, Elements, Result) :- length(EmptyList, Num), populate_list(EmptyList, [add, subtract, multiply, divide], Result).
+
+combo(Operators, Numbers, Result) :- length(Numbers, L), Len is L - 1, allLists(Len, Operators, PossibleOperations), apply_lr(PossibleOperations, Numbers, Result).
