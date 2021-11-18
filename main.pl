544c00148f57a08e3d7229542e2917876f73dbba
Avery Brinkman <71273469+Avery-Brinkman@users.noreply.github.com>
2021-11-18 00:09:23 -0500

    Submitted version

--- main.pl
+++ main.pl
@@ -1,7 +1,7 @@
-apply(add, X, Y, Result) :- Result is X + Y, !.
-apply(subtract, X, Y, Result) :- Result is X - Y, !.
-apply(multiply, X, Y, Result) :- Result is X * Y, !.
-apply(divide, X, Y, Result) :- Result is X / Y, !.
+apply(add, X, Y, Result) :- Result is X + Y.
+apply(subtract, X, Y, Result) :- Result is X - Y.
+apply(multiply, X, Y, Result) :- Result is X * Y.
+apply(divide, X, Y, Result) :- Result is X / Y.
 
 apply_lr([], [Result], Result).
 apply_lr([Op|OpTail], [X, Y |NumTail], FinalResult) :- apply(Op, X, Y, Result), apply_lr(OpTail, [Result|NumTail], FinalResult).	
@@ -9,7 +9,7 @@ apply_lr([Op|OpTail], [X, Y |NumTail], FinalResult) :- apply(Op, X, Y, Result), 
 populate_list([], _, []).
 populate_list([_|Xs], Elements, NewList) :- append(_, [E|_], Elements), populate_list(Xs, Elements, NewTail), append([E], NewTail, NewList).
 
-allLists(Num, Elements, Result) :- ground(Elements), !, length(EmptyList, Num), populate_list(EmptyList, Elements, Result).
-allLists(Num, Elements, Result) :- length(EmptyList, Num), populate_list(EmptyList, [add, subtract, multiply, divide], Result).
+all_lists(Num, Elements, Result) :- ground(Elements), !, length(EmptyList, Num), populate_list(EmptyList, Elements, Result).
+all_lists(Num, Elements, Result) :- length(EmptyList, Num), populate_list(EmptyList, [add, subtract, multiply, divide], Result).
 
-combo(Operators, Numbers, Result) :- length(Numbers, L), Len is L - 1, allLists(Len, Operators, Operators), apply_lr(Operators, Numbers, Result).
+combo(Operators, Numbers, Result) :- length(Numbers, L), Len is L - 1, all_lists(Len, Operators, Operators), apply_lr(Operators, Numbers, Result).
